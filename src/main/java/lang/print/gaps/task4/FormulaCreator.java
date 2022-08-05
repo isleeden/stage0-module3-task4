7de7816c8178857a83d9f38606f5ffca98c0a793
Denis Lee <51025257+isleeden@users.noreply.github.com>
2022-08-06 02:06:53 +0500

    Update FormulaCreator.java

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        float a = 1f;
+        float b = 3f;
+        System.out.println((9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2));
     }
 }
